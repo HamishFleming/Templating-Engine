0157b54ff467e6ca80f40dbaf965ee499150ab4a
HamishFleming <50164339+HamishFleming@users.noreply.github.com>
2023-01-08 13:06:31 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
 #!/usr/bin/env node
-
-
 import * as fs from 'fs';
 import * as path from 'path';
 import * as inquirer from 'inquirer';
 
-import chalk from 'chalk';
+// import chalk from 'chalk';
 
 const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
 const QUESTIONS = [
-{
-   name: 'template',
-   type: 'list',
-   message: 'What project template would you like to use?',
-   choices: CHOICES
-},
-{
-   name: 'name',
-   type: 'input',
-   message: 'New project name?'
-}];
+  {
+    name: 'template',
+    type: 'list',
+    message: 'What project template would you like to use?',
+    choices: CHOICES,
+  },
+  {
+    name: 'name',
+    type: 'input',
+    message: 'New project name?',
+  },
+];
 
-inquirer.prompt(QUESTIONS)
-    .then((answers: any) => {
-        console.log(answers);
-    });
-
+inquirer.prompt(QUESTIONS).then((answers: any) => {
+  console.log(answers);
+});
