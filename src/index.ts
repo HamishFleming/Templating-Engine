7f5927963d295cfa9d6a5e93ed8dbf690dd30433
HamishFleming <50164339+HamishFleming@users.noreply.github.com>
2023-01-08 11:34:00 +0000

    wip

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as inquirer from 'inquirer';
+
+import chalk from 'chalk';
+
+const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+const QUESTIONS = [
+{
+   name: 'template',
+   type: 'list',
+   message: 'What project template would you like to use?',
+   choices: CHOICES
+},
+{
+   name: 'name',
+   type: 'input',
+   message: 'New project name?'
+}];
+
+inquirer.prompt(QUESTIONS)
+    .then((answers: any) => {
+        console.log(answers);
+    });
+
